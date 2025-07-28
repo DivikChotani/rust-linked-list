fa13686181fd1d2fd06d794a8e3b2b182e2fb918
Divik Chotani <divik.chotani@gmail.com>
2025-07-28 14:14:50 -0700

    handled drop for stack

--- src/third.rs
+++ src/third.rs
@@ -34,8 +34,40 @@ impl<T> List<T>{
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
     }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {next: self.head.as_deref()}
+    }
+
 }
 
+
+pub struct Iter<'a, T>{
+    next: Option<&'a Node<T>>
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node|{ 
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_node = self.head.take();
+        while let Some(node) = cur_node {
+            if let Ok(mut cur) = Rc::try_unwrap(node) {
+                cur_node = cur.next.take();
+            } else{
+                break;
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -63,4 +95,15 @@ mod test {
         assert_eq!(list.head(), None);
 
     }
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
 }
+
