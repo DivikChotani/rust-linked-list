00e07bf06b20192dc72f2a807c47574065f50ee0
Divik Chotani <divik.chotani@gmail.com>
2025-07-27 19:00:41 -0700

    tests and dropping

--- src/first.rs
+++ src/first.rs
@@ -34,6 +34,16 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut node) = cur_link {
+            cur_link = mem::replace(&mut node.next, Link::Empty);
+        }
+    }
+}
+
 pub enum Link{
     Empty,
     More(Box<Node>)
@@ -42,4 +52,39 @@ pub enum Link{
 pub struct Node{
     val: i32,
     next: Link
+}
+
+#[cfg(test)]
+mod test {
+    use crate::first::List;
+
+    #[test]
+    fn basics() {
+
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }
