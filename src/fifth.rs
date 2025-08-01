924b361f4f4bd87bebddf9345e1eb8355eb4b2c9
Divik Chotani <divik.chotani@gmail.com>
2025-08-01 13:27:31 -0700

    minor bug fix after testing

--- src/fifth.rs
+++ src/fifth.rs
@@ -24,11 +24,11 @@ impl<T> List<T> {
                     Node {elem: elem, next: ptr::null_mut()}
             ));
 
-            if self.tail.is_null() {
-                self.head = node;
+            if !self.tail.is_null() {
+                (*self.tail).next = node;
             }
             else {
-                (*self.tail).next = node;
+                self.head = node;
             }
             self.tail = node;
         }
@@ -36,14 +36,14 @@ impl<T> List<T> {
 
     pub fn pop(&mut self) -> Option<T> {
         unsafe {
-            if self.tail.is_null() {
+            if self.head.is_null() {
                 None
             }
             else {
                 let head = Box::from_raw(self.head);
                 self.head = head.next;
                 
-                if (*self.head).next.is_null() {
+                if self.head.is_null() {
                     self.tail = ptr::null_mut();
                 }
                 Some(head.elem)
@@ -57,3 +57,47 @@ impl<T> Drop for List<T> {
         while let Some(_) = self.pop() {}
     }
 } 
+
+
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
+    }
+}
